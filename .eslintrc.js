aa73b0789259e10eea5a0ad9fdff14e3a3d320e3
Okkun555 <y.okuno.btm@gmail.com>
2023-02-04 23:37:38 +0900

    chore:eslint, prettierの設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,28 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript"],
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'next/core-web-vitals',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+    'prettier',
+  ],
   overrides: [],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
-  rules: {},
-};
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
+}
