5a2e66d66e34251cab4aef032aa0e65a3f11cff2
Okkun555 <y.okuno.btm@gmail.com>
2023-02-18 10:55:38 +0900

    feat:認証画面の雛形を作成

---
+++ src/components/feature/auth/presenter/SignInForm.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react'
+import { AuthFormType } from '../type'
+
+type SignInFormProps = {
+  formTitle: string
+}
+
+const SignInForm: FC<SignInFormProps> = ({ formTitle }) => {
+  return (
+    <>
+      <h1>form</h1>
+    </>
+  )
+}
+
+export default SignInForm
---
+++ src/pages/signin.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+
+const SignIn: NextPage = () => {
+  return (
+    <>
+      <h1>これはログインページです</h1>
+    </>
+  )
+}
+
+export default SignIn
